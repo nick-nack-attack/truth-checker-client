2d1a34938b0ec70d96f23cd231512322baa80508
Nick Nack Attack <nickfarni001@gmail.com>
2021-03-04 16:22:52 -0700

    • added 5 new tests to button component

--- src/components/utils/Button/Button.test.js
+++ src/components/utils/Button/Button.test.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Button from './Button';
 
-import { UserContextProvider } from '../../../contexts/UserContext'
+import { UserContextProvider } from '../../../contexts/UserContext';
 import AuthContextProvider from '../../../contexts/AuthContextProvider';
+import { shallow, mount } from 'enzyme';
 
 describe('Button Component', () => {
 
     it('renders without crashing', () => {
     const div = document.createElement('div');
-    
+
     ReactDOM.render(
         <Router>
             <UserContextProvider>
                 <AuthContextProvider>
-                    <Button/>
+                    <Button text={'test'}/>
                 </AuthContextProvider>
             </UserContextProvider>
         </Router>,
@@ -24,5 +25,22 @@ describe('Button Component', () => {
 
     ReactDOM.unmountComponentAtNode(div);
     });
+
+    it('renders with attributes', () => {
+        expect(shallow(<Button type={"submit"}/>).contains(<button type="submit"/>)).toBe(true);
+    });
+
+    it('should mount in a full DOM', () => {
+        expect(mount(<Button/>).find('button').length).toBe(1);
+    });
+
+    it('should render to static HTML', () => {
+        expect(mount(<Button text={'Submit'}/>).text()).toEqual('Submit')
+    });
+
+    it('can be clicked', () => {
+        const wrapper = shallow(<Button/>);
+        wrapper.find('button').simulate('click', {preventDefault() {}})
+    })
 
 });
